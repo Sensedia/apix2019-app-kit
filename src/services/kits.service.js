264f8d0ec7136eda3dac42fe1fd3d9337e71688b
Yago Dórea <yago.dorea@sensedia.com>
2019-05-20 13:35:45 -0300

    Adds some comment to the service to be filled.

--- src/services/kits.service.js
+++ src/services/kits.service.js
@@ -1,8 +1,15 @@
+import axios from "axios";
 
 export const kitsService = {
     submitKit
 }
 
+/**
+ * Essa função é a que invoca o serviço externo, a API de Kits.
+ * Use o Axios ou outra biblioteca de requisições HTTP para fazer
+ * aqui a requisição. Retorne uma 'Promise' (o resultado das requisições
+ * axios são Promises!)
+ */
 function submitKit(kit) {
     return new Promise(resolve => {
         setTimeout(() => {
